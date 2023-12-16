6f2ada96b70def9b7bd2b3f537ad382e74504894
Mark Herrmann <mark.herrmann@mailbox.org>
2023-12-16 22:42:17 +0100

    feat: builder

--- index.js
+++ index.js
@@ -2,8 +2,63 @@
 
 import { program } from 'commander';
 import chalk from 'chalk';
+import mermaidParse from 'mermaid-parse';
+import fs from 'fs/promises';
+
+const mdToDiagram = async function(md) {
+  const definition = md.replace(/^```mermaid\s(.*)```$/s, '$1');
+  return await mermaidParse(definition, { extension: 'svg' });
+};
+
+const fileExists = async function (path) {
+  let exists;
+  fs.stat(path)
+    .then(() => { exists = true })
+    .catch(() => { exists = false });
+  return exists;
+}
+
+const checkDirectories = async function (input, output) {
+  const inputExists = await fileExists(input);
+  const outputExists = await fileExists(output);
+  const inputStats = inputExists && await fs.stat(input);
+  const outputStats = outputExists && await fs.stat(output);
+  if (!inputExists || !inputStats.isDirectory()) {
+    console.error(chalk.red.bold('Error: input does not exist or is not a directory.'));
+    process.exit(1);
+  }
+  if (!outputExists || !outputStats.isDirectory()) {
+    console.error(chalk.red.bold('Error: output does not exist or is not a directory.'));
+    process.exit(1);
+  }
+};
+
+const buildFiles = async function (input, output, all, verbose) {
+  await checkDirectories(input, output);
+
+  const files = await fs.readdir(input);
+  files.forEach(async (fileName) => {
+    const filePath = `${input}/${fileName}`;
+    const fileStats = await fs.stat(filePath);
+    const diagramPath = `${output}/${fileName.replace(/\.md$/, '.svg')}`;
+    const diagramExists = await fileExists(diagramPath);
+    const skip = diagramExists && !all;
+    if (!fileStats.isFile() || !fileName.endsWith('.md') || skip) {
+      return;
+    }
+    if (verbose) console.log(chalk.bold(fileName));
+    const md = await fs.readFile(filePath, 'utf8');
+    const diagram = await mdToDiagram(md);
+    await fs.writeFile(diagramPath, diagram, 'utf8');
+  });
+};
 
 const build = async function (...args) {
+  const [input, output, options] = args;
+  const all = options.all ?? false;
+  const verbose = options.verbose ?? false;
+
+  await buildFiles(input, output, all, verbose);
 };
 
 program
